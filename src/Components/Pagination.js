6f41ed29c2ad7a9da7a1bd3b31d7c632792f77ad
Eduardo de Souza Magalhaes <84235466+edua2127@users.noreply.github.com>
2022-12-16 13:35:58 -0300

    desenvolvimento da paginacao

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -6,19 +6,30 @@ function range(start, end) {
     .map((_, idx) => start + idx);
 }
 
-
-const Pagination = ({ quantidade_de_items_por_pagina, total_de_items, starts, setStarts }) => {
-  const quantidade_de_paginas = Math.ceil(total_de_items / quantidade_de_items_por_pagina);
+const Pagination = ({
+  quantidade_de_items_por_pagina,
+  total_de_items,
+  starts,
+  setStarts,
+}) => {
+  const quantidade_de_paginas = Math.ceil(
+    total_de_items / quantidade_de_items_por_pagina
+  );
   const pagina_atual = starts / quantidade_de_items_por_pagina + 1;
   const paginas_mostradas = range(pagina_atual - 2, pagina_atual + 2).filter(
     (pagina) => pagina > 0 && pagina <= quantidade_de_paginas
   );
   return (
     <ul className={style.botoes_paginacao}>
-      { paginas_mostradas.map((pagina) => {
+      {paginas_mostradas.map((pagina) => {
         return (
           <li key={pagina}>
-            <button className={style.botao_da_pagina} onClick={() => setStarts((pagina - 1) * quantidade_de_items_por_pagina)}>
+            <button
+              className={style.botao_da_pagina}
+              onClick={() =>
+                setStarts((pagina - 1) * quantidade_de_items_por_pagina)
+              }
+            >
               {pagina}
             </button>
           </li>
